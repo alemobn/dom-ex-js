5369d399bc4e5a8b6801be5437fb33b68225fc89
ale <alemoura.zyc@gmail.com>
2024-10-06 06:12:41 -0300

    style: update frontend

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const paragrafos = document.querySelector('.paragrafos');
 const paragrafosTags = paragrafos.querySelectorAll('p');
-const title = document.querySelector('h1');
 
 const estilosBody = getComputedStyle(document.body);
 const backgroundColorBody = estilosBody.backgroundColor;
 const estilosContainer = getComputedStyle(document.querySelector('.container'));
 const backgroundColorContainer = estilosContainer.backgroundColor;
-
-title.style.textAlign = 'center';
+const fontColor = estilosBody.getPropertyValue('--font-color');
 
 for (let p of paragrafosTags) {
     p.style.backgroundColor = backgroundColorBody;
-    p.style.color = backgroundColorContainer;
+    p.style.color = fontColor;
     p.style.borderRadius = '15px';
     p.style.padding = '10px';
+    p.style.width = '100%'
 }
